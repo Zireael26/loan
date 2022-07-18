d94d894d68ca4e49f5caee06b9c7fed5ceeffe58
Abhishek Kaushik <abhishek.nexus26@gmail.com>
2022-07-18 09:20:20 +0530

    Added ValidateBasic() logic for request loan message

--- x/loan/types/message_request_loan.go
+++ x/loan/types/message_request_loan.go
@@ -45,5 +45,26 @@ func (msg *MsgRequestLoan) ValidateBasic() error {
 	if err != nil {
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
 	}
+
+	amount, _ := sdk.ParseCoinsNormalized(msg.Amount)
+	fee, _ := sdk.ParseCoinsNormalized(msg.Fee)
+	collateral, _ := sdk.ParseCoinsNormalized(msg.Collateral)
+
+	if !amount.IsValid() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is not a valid Coins object")
+	}
+	if amount.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "amount is empty")
+	}
+	if !fee.IsValid() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "fee is not a valid Coins object")
+	}
+	if !collateral.IsValid() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is not a valid Coins object")
+	}
+	if collateral.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "collateral is empty")
+	}
+
 	return nil
 }
